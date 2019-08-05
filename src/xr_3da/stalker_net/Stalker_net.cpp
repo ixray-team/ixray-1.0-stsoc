492c4d246841194a686c702e07048dc20ae80f80
acidicMercury8 <acidicmercury8@outlook.com>
2019-08-05 22:51:11 +0300

    Fix resources reading in Stalker_net

--- src/xr_3da/stalker_net/Stalker_net.cpp
+++ src/xr_3da/stalker_net/Stalker_net.cpp
@@ -33,6 +33,61 @@ CStalker_netApp::~CStalker_netApp()
 
 CStalker_netApp theApp;
 
+// Calculating build number from XR_3DA for correct resources reading
+
+// computing build id
+XRCORE_API	LPCSTR	build_date;
+XRCORE_API	u32		build_id;
+
+//#define NO_SINGLE
+#define NO_MULTI_INSTANCES
+
+static LPSTR month_id[12] = {
+	"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"
+};
+
+static int days_in_month[12] = {
+	31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+};
+
+static int start_day	= 31;	// 31
+static int start_month	= 1;	// January
+static int start_year	= 1999;	// 1999
+
+void compute_build_id	()
+{
+	build_date			= __DATE__;
+
+	int					days;
+	int					months = 0;
+	int					years;
+	string16			month;
+	string256			buffer;
+	strcpy_s				(buffer,__DATE__);
+	sscanf				(buffer,"%s %d %d",month,&days,&years);
+
+	for (int i=0; i<12; i++) {
+		if (_stricmp(month_id[i],month))
+			continue;
+
+		months			= i;
+		break;
+	}
+
+	build_id			= (years - start_year)*365 + days - start_day;
+
+	for (int i=0; i<months; ++i)
+		build_id		+= days_in_month[i];
+
+	for (int i=0; i<start_month-1; ++i)
+		build_id		-= days_in_month[i];
+}
+
+typedef void DUMMY_STUFF (const void*,const u32&,void*);
+XRCORE_API DUMMY_STUFF	*g_temporary_stuff;
+
+#define TRIVIAL_ENCRYPTOR_DECODER
+#include "..\trivial_encryptor.h"
 
 // CStalker_netApp initialization
 
@@ -44,6 +99,10 @@ BOOL CStalker_netApp::InitInstance()
 	InitCommonControls();
 
 	CWinApp::InitInstance();
+
+	// Decoding resources and calculating build number
+	g_temporary_stuff = trivial_encryptor::decode;
+	compute_build_id();
 
 	Core._initialize("Stalker_net", NULL, TRUE);
 	
