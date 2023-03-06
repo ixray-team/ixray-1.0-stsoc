f6d80ffcb24378e11d3030c19301a779cef44f53
Drombeys <dima.zolotov02@gmail.com>
2023-03-07 01:38:51 +0300

    Delete `DEFINE_STACK()` macros

--- src/xrCore/_stl_extensions.h
+++ src/xrCore/_stl_extensions.h
@@ -122,9 +122,6 @@ struct pred_stri	: public std::binary_function<char*, char*, bool>	{
 	IC bool operator()(const char* x, const char* y) const				{	return _stricmp(x,y)<0;	}
 };
 
-// STL extensions
-#define DEFINE_STACK(T,N)			typedef xr_stack< T > N;
-
 #include "FixedVector.h"
 
 // auxilary definition
