9b6887f3d7e048477e20625a49b8a39170bc0350
MAYLAYSHEZ <MAYLAYSHEZ@yandex.ru>
2023-08-25 22:57:56 +0300

    Enable output log for debugger connected

--- src/xrCore/log.cpp
+++ src/xrCore/log.cpp
@@ -47,10 +47,10 @@ void AddOne				(const char *split)
 
 	logCS.Enter			();
 
-#ifdef DEBUG
-	OutputDebugString	(split);
-	OutputDebugString	("\n");
-#endif
+	if (IsDebuggerPresent()) {
+		OutputDebugString(split);
+		OutputDebugString("\n");
+	}
 
 //	DUMP_PHASE;
 	{
